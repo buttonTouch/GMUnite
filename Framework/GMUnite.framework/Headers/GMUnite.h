46912a5e238304d03c9ed1223dfb45a865557e52
MWS <18824676521@163.com>
2021-04-23 14:32:57 +0800

    66666

--- Framework/GMUnite.framework/Headers/GMUnite.h
+++ Framework/GMUnite.framework/Headers/GMUnite.h
@@ -2,7 +2,7 @@
 //  GMUnite.h
 //  GMUnite
 //
-//  Created by Mgs on 2021/4/22.
+//  Created by Mgs on 2021/4/23.
 //
 
 #import <Foundation/Foundation.h>
@@ -14,7 +14,5 @@ FOUNDATION_EXPORT double GMUniteVersionNumber;
 FOUNDATION_EXPORT const unsigned char GMUniteVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <GMUnite/PublicHeader.h>
-#import <GMUnite/GMTools.h>
-#import <GMUnite/DeviceId.h>
 
 
